735cc4210d01d1d83095b4ab315f033695af58a7
Thang Nguyen <nguyenhongthang1998@gmail.com>
2019-07-20 21:51:05 -0400

    Change css margin-top

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BACKEND_URL: 'https://shareitonline.azurewebsites.net',
+  BACKEND_URL: 'https://api.shareit.works',
   MAX_FILE_SIZE: 10000000
 };
